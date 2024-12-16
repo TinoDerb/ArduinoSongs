d377b296b24024e4bb765f3b28952da0f563ef47
Tino D <81869266+TinoDerb@users.noreply.github.com>
2024-12-16 12:24:21 +0100

    Update greasyStrangler.ino

--- greasyStrangler/greasyStrangler.ino
+++ greasyStrangler/greasyStrangler.ino
@@ -9,23 +9,63 @@ int quarter = 250; // 4
 int half = 500; // 2
 int whole = 750; // 1
 int durations[] = {
-  quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter, 
+  quarter, quarter, quarter, quarter, quarter, quarter, eighth,  eighth, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, eighth,  eighth, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, eighth,  eighth, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, eighth,  eighth, quarter,
   quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter,
   quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter,
   quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter,
   quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter,
   quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter,
-  quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter
+  quarter, quarter, quarter, quarter, quarter, quarter, eighth,  eighth, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, eighth,  eighth, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, eighth,  eighth, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, eighth,  eighth, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, quarter, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, quarter,
+  quarter, quarter, quarter, quarter, quarter, quarter, whole, quarter, whole
 };
-
 int melody[] = {
-  NOTE_G4, NOTE_F4, NOTE_G4, NOTE_AS4, NOTE_DS4, NOTE_F4, NOTE_G4, NOTE_DS4,
-  NOTE_D4, NOTE_DS4, NOTE_F4, NOTE_D4, NOTE_DS4, NOTE_F4, NOTE_G4, 0,
-  NOTE_G4, NOTE_F4, NOTE_G4, NOTE_AS4, NOTE_DS4, NOTE_F4, NOTE_G4, NOTE_DS4,
-  0, NOTE_F4, NOTE_A4, NOTE_G4, NOTE_DS4, NOTE_D4, NOTE_DS4, NOTE_F4,
-  NOTE_D4, NOTE_G4, NOTE_A4, NOTE_G4, NOTE_DS4, NOTE_AS4, NOTE_DS4, NOTE_G4,
-  NOTE_F4, NOTE_G4, NOTE_DS4, NOTE_F4, NOTE_G4, NOTE_F4, NOTE_DS4, 0,
-  NOTE_DS4, NOTE_D4, NOTE_C4, NOTE_AS3, NOTE_DS4, NOTE_D4, NOTE_C4, 0
+  NOTE_AS2, NOTE_DS3, NOTE_AS2, NOTE_DS3, NOTE_AS2, NOTE_DS3, NOTE_AS2, NOTE_AS2, 0,
+  NOTE_AS2, NOTE_DS3, NOTE_AS2, NOTE_DS3, NOTE_AS2, NOTE_GS2, NOTE_GS2, 0,
+  NOTE_AS2, NOTE_DS3, NOTE_AS2, NOTE_DS3, NOTE_AS2, NOTE_DS3, NOTE_AS2, NOTE_AS2, 0,
+  NOTE_AS2, NOTE_DS3, NOTE_AS2, NOTE_DS3, NOTE_AS2, NOTE_A3,  NOTE_A3,  0,
+  NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_AS1, 0,
+  NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_GS1, NOTE_GS1, 0,
+  NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_AS1, 0,
+  NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_A2,  NOTE_A2,  0,
+  NOTE_A3,  NOTE_B3,  NOTE_C4,  NOTE_B3,  NOTE_A3,  NOTE_B3,  NOTE_A3,  0,
+  NOTE_A3,  NOTE_B3,  NOTE_C4,  NOTE_B3,  NOTE_A3,  NOTE_D4,  NOTE_D4,  0,
+  NOTE_A4,  NOTE_B4,  NOTE_C5,  NOTE_B4,  NOTE_A4,  NOTE_B4,  NOTE_A4,  0,
+  NOTE_A4,  NOTE_B4,  NOTE_C5,  NOTE_B4,  NOTE_A4,  NOTE_D5,  NOTE_D5,  0,
+  NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_AS1, 0,
+  NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_GS1, NOTE_GS1, 0,
+  NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_AS1, 0,
+  NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_A2,  NOTE_A2,  0,
+  NOTE_DS3, NOTE_CS3, NOTE_DS3, NOTE_F3,  NOTE_DS3, NOTE_CS3, NOTE_CS3, 0,
+  NOTE_DS3, NOTE_CS3, NOTE_DS3, NOTE_F3,  NOTE_DS3, NOTE_CS3, NOTE_DS3, 0,
+  NOTE_DS3, NOTE_CS3, NOTE_DS3, NOTE_F3,  NOTE_DS3, NOTE_CS3, NOTE_CS3, 0,
+  NOTE_DS3, NOTE_CS3, NOTE_DS3, NOTE_F3,  NOTE_DS3, NOTE_A3,  NOTE_A3,  0,
+  NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_AS1, 0,
+  NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_GS1, NOTE_GS1, 0,
+  NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_AS1, 0,
+  NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_DS2, NOTE_AS1, NOTE_A2,  NOTE_A2,  0,
+  NOTE_AS1, NOTE_DS2, NOTE_A2,  NOTE_AS2, NOTE_F2,  NOTE_AS1, NOTE_C3,  0,
+  NOTE_AS1, NOTE_DS2, NOTE_B2,  NOTE_C3,  NOTE_AS2, NOTE_DS3, 0,
+  NOTE_AS1, NOTE_DS2, NOTE_GS2, NOTE_AS2, NOTE_F2,  NOTE_AS1, 0,
+  NOTE_G2,  NOTE_F2,  NOTE_DS2, NOTE_AS1, NOTE_AS1, 0,        NOTE_B0,  0,         NOTE_B0
 };
 
 void setup() { // setup all the outputs to pinMode
